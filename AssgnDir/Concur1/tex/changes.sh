62b683307c862aee0d7a79edbf93379de03cd2e2
aoleson <alannah313@gmail.com>
2017-10-08 20:59:44 -0700

    Modified git log formatting

--- AssgnDir/Concur1/tex/changes.sh
+++ AssgnDir/Concur1/tex/changes.sh
@@ -9,7 +9,11 @@ origin=`git config remote.origin.url`
 base=`dirname "$origin"`/`basename "$origin" .git`
 
 # Output LaTeX table in chronological order
+echo "\\begin{table}[ht]"
+echo "\\centering"
+echo "\\resizebox{\textwidth}{!}{"
 echo "\\begin{tabular}{l l l}\\textbf{Detail} & \\textbf{Author} & \\textbf{Description}\\\\\\hline"
 git log --pretty=format:"\\href{$base/commit/%H}{%h} & %an & %s\\\\\\hline" --reverse
-echo "\end{tabular}"
+echo "\\end{tabular}}"
+echo "\\end{table}"
 
