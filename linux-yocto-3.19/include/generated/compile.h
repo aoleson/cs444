7b52fac12ea7961bf1952ea5fe1ad9dbfa367576
aoleson <alannah313@gmail.com>
2017-11-15 22:34:14 -0800

    finishing a3

--- linux-yocto-3.19/include/generated/compile.h
+++ linux-yocto-3.19/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "i386"
-#define UTS_VERSION "#12 SMP PREEMPT Tue Nov 14 16:05:03 PST 2017"
+#define UTS_VERSION "#14 SMP PREEMPT Tue Nov 14 18:57:17 PST 2017"
 #define LINUX_COMPILE_BY "olesona"
 #define LINUX_COMPILE_HOST "os2.engr.oregonstate.edu"
-#define LINUX_COMPILER "gcc version 4.9.2 (GCC) "
+#define LINUX_COMPILER "gcc version 4.8.5 20150623 (Red Hat 4.8.5-11) (GCC) "
